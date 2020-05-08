5e8ff3d47f9fdceffda1d56f11b3a8059abcbbbf
Pekka Maanpää <pekkamaa@vaadin.com>
2020-05-08 15:40:33 +0300

    Revert the waitUntil

--- vaadin-menu-bar-flow-integration-tests/src/test/java/com/vaadin/flow/component/menubar/tests/MenuBarPageIT.java
+++ vaadin-menu-bar-flow-integration-tests/src/test/java/com/vaadin/flow/component/menubar/tests/MenuBarPageIT.java
@@ -38,8 +38,6 @@ public class MenuBarPageIT extends AbstractComponentIT {
     @Before
     public void init() {
         open();
-        waitUntil(driver -> findElements(By.tagName("vaadin-menu-bar"))
-                .size() > 0, 300);
         menuBar = $(MenuBarElement.class).first();
     }
 
